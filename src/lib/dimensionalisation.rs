b493ca01131d037d745658d49137cdf2d906492d
teddy0@archIdeaPadDesktop <tong011@e.ntu.edu.sg>
2022-09-14 19:12:16 -0700

    converted to and from Be and Re, yet to test...

--- src/lib/dimensionalisation.rs
+++ src/lib/dimensionalisation.rs
@@ -5,6 +5,8 @@ use uom::si::mass_rate::kilogram_per_second;
 use uom::si::area::square_meter;
 use uom::si::length::{meter,centimeter,foot,inch};
 use uom::si::f64::*;
+use uom::typenum::P2;
+
 // uom stands for unit of measure.
 //
 // this set of functions here is simply to convert to and from
@@ -13,16 +15,27 @@ use uom::si::f64::*;
 struct CalcReynolds {}
 impl CalcReynolds {
     #[allow(non_snake_case)]
-    pub fn from_velocity(rho: MassDensity,
+    pub fn from_velocity(fluidDensity: MassDensity,
                      velocity: Velocity, 
                      hydraulic_diameter: Length,
-                     mu: DynamicViscosity) -> f64 {
+                     fluidViscosity: DynamicViscosity) -> f64 {
+
+        if fluidViscosity.value <= 0.0 {
+            panic!("fluid Viscosity <= 0.0, nonphysical");
+        }
+
+        if hydraulic_diameter.value <= 0.0 {
+            panic!("hydraulic Diameter <= 0.0, nonphysical");
+        }
+        if fluidDensity.value <= 0.0 {
+            panic!("fluidDensity <= 0.0, nonphysical");
+        }
 
         let reynolds_number = 
-            rho * 
+            fluidDensity * 
             velocity * 
             hydraulic_diameter / 
-            mu;
+            fluidViscosity;
 
         return reynolds_number.value.into();
 
@@ -53,4 +66,91 @@ impl CalcReynolds {
 
         return reynolds_number.value.into();
     }
+
+    
+    #[allow(non_snake_case)]
+    pub fn to_mass_rate(crossSectionalArea: Area,
+                        Re: f64,
+                        hydraulicDiameter: Length,
+                        fluidViscosity: DynamicViscosity) -> MassRate {
+
+        if fluidViscosity.value <= 0.0 {
+            panic!("fluid Viscosity <= 0.0, nonphysical");
+        }
+
+        if hydraulicDiameter.value <= 0.0 {
+            panic!("hydraulic Diameter <= 0.0, nonphysical");
+        }
+
+        if crossSectionalArea.value <= 0.0 {
+            panic!("pipe Area <= 0.0, nonphysical");
+        }
+
+        let fluidMassFlowrate = fluidViscosity*
+            crossSectionalArea/
+            hydraulicDiameter*
+            Re;
+
+        return fluidMassFlowrate;
+    }
+}
+
+struct CalcBejan {}
+impl CalcBejan {
+
+    #[allow(non_snake_case)]
+    pub fn from_pressure(fluidPressure: Pressure,
+              hydraulicDiameter: Length,
+              fluidDensity: MassDensity,
+              fluidViscosity: DynamicViscosity) -> f64 {
+
+
+        if fluidViscosity.value <= 0.0 {
+            panic!("fluid Viscosity <= 0.0, nonphysical");
+        }
+
+        if hydraulicDiameter.value <= 0.0 {
+            panic!("hydraulic Diameter <= 0.0, nonphysical");
+        }
+
+        if fluidDensity.value <= 0.0 {
+            panic!("fluidDensity <= 0.0, nonphysical");
+        }
+
+        let Be = fluidPressure*
+            fluidDensity *
+            hydraulicDiameter.powi(P2::new())/
+            fluidViscosity.powi(P2::new());
+
+        return Be.value.into();
+    }
+
+    #[allow(non_snake_case)]
+    pub fn to_pressure(Be_D: f64,
+                       hydraulicDiameter: Length,
+                       fluidDensity: MassDensity,
+                       fluidViscosity: DynamicViscosity) -> Pressure {
+
+
+        if fluidViscosity.value <= 0.0 {
+            panic!("fluid Viscosity <= 0.0, nonphysical");
+        }
+
+        if hydraulicDiameter.value <= 0.0 {
+            panic!("hydraulic Diameter <= 0.0, nonphysical");
+        }
+
+        if fluidDensity.value <= 0.0 {
+            panic!("fluidDensity <= 0.0, nonphysical");
+        }
+
+        let fluidPressure = fluidViscosity.powi(P2::new())*
+                        Be_D/
+                        hydraulicDiameter.powi(P2::new())/
+                        fluidDensity;
+
+        return fluidPressure;
+    }
+
+
 }
