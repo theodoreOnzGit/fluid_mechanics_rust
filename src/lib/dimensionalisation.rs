53a3dcf96079382df259d6d5ee38b73ddcdbd85d
teddy0@archIdeaPadDesktop <tong011@e.ntu.edu.sg>
2022-09-14 18:47:44 -0700

    reynolds number calc methods added

--- src/lib/dimensionalisation.rs
+++ src/lib/dimensionalisation.rs
@@ -9,6 +9,48 @@ use uom::si::f64::*;
 //
 // this set of functions here is simply to convert to and from
 // dimensionless numbers to SI units, and back
-// 
-//
 
+struct CalcReynolds {}
+impl CalcReynolds {
+    #[allow(non_snake_case)]
+    pub fn from_velocity(rho: MassDensity,
+                     velocity: Velocity, 
+                     hydraulic_diameter: Length,
+                     mu: DynamicViscosity) -> f64 {
+
+        let reynolds_number = 
+            rho * 
+            velocity * 
+            hydraulic_diameter / 
+            mu;
+
+        return reynolds_number.value.into();
+
+    }
+
+
+    #[allow(non_snake_case)]
+    pub fn from_mass_rate(fluidMassFlowrate: MassRate,
+                        crossSectionalArea: Area,
+                        hydraulic_diameter: Length,
+                        fluidViscosity: DynamicViscosity) -> f64 {
+
+        if fluidViscosity.value <= 0.0 {
+            panic!("fluid Viscosity <= 0.0, nonphysical");
+        }
+
+        if hydraulic_diameter.value <= 0.0 {
+            panic!("hydraulic Diameter <= 0.0, nonphysical");
+        }
+        if crossSectionalArea.value <= 0.0 {
+            panic!("pipe Area <= 0.0, nonphysical");
+        }
+
+        let reynolds_number = fluidMassFlowrate/
+            crossSectionalArea*
+            hydraulic_diameter/
+            fluidViscosity;
+
+        return reynolds_number.value.into();
+    }
+}
