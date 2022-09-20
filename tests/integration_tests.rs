dbe2df21cbb0d353b285103a9c9ff51edd75929b
teddy0 <teddy0@windowsDesktopThLabOffice>
2022-09-20 11:48:45 -0700

    ctah results verified to be within 4% of expt data

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -36,7 +36,7 @@ pub fn when_ctah_pressure_change_expect_correct_value_zero_flow(){
     let ctah_vertical = factory::CTAHVertical::get();
     let ctah_horizontal = factory::CTAHHorizontal::get();
 
-    // now let's have a temperature of 21C and mass flow of 0.15 kg/s
+    // now let's have a temperature of 20C and mass flow of 0.15 kg/s
     let fluid_temp = ThermodynamicTemperature::new::<
         degree_celsius>(20.0);
     let mass_flow_expected = MassRate::new::<kilogram_per_second>(0.0);
@@ -88,57 +88,68 @@ pub fn when_ctah_pressure_change_expect_correct_value_non_zero_flow(){
     use uom::si::f64::*;
 
 
-    // let's get the component for ctah
-    let ctah_vertical = factory::CTAHVertical::get();
-    let ctah_horizontal = factory::CTAHHorizontal::get();
+    for i in 0..19 {
+        // Setup
+        // let's get the component for ctah
+        let ctah_vertical = factory::CTAHVertical::get();
+        let ctah_horizontal = factory::CTAHHorizontal::get();
+        // now let's have a temperature of 20C and mass flow of 0.15 kg/s
+        let fluid_temp = ThermodynamicTemperature::new::<
+            degree_celsius>(20.0);
+        let mass_flow_expected = MassRate::new::
+            <kilogram_per_second>(0.01* i as f64);
+        // let's get the pressure changes
+        use fluid_mechanics_rust::therminol_component::
+            CalcPressureChange;
 
-    // now let's have a temperature of 21C and mass flow of 0.15 kg/s
-    let fluid_temp = ThermodynamicTemperature::new::<
-        degree_celsius>(20.0);
-    let mass_flow_expected = MassRate::new::<kilogram_per_second>(0.18);
+        let ctah_reference_pressure_change = 
+            get_ctah_pressure_change_empirical(
+                mass_flow_expected,
+                fluid_temp);
 
-    // let's get the pressure changes
-    use fluid_mechanics_rust::therminol_component::CalcPressureChange;
+        let expected_pressure_loss = -(ctah_reference_pressure_change.
+                                       value);
+
+        // Act
+        let ctah_vertical_pressure_change = 
+            CalcPressureChange::from_mass_rate(
+                &ctah_vertical,
+                mass_flow_expected,
+                fluid_temp);
+
+        let ctah_horizontal_pressure_change = 
+            CalcPressureChange::from_mass_rate(
+                &ctah_horizontal,
+                mass_flow_expected,
+                fluid_temp);
+
+        // let's subtract out the pressure change due to hydrostatic pressure
+        //
+        use fluid_mechanics_rust::therminol_component::
+            StandardCustomComponentProperties;
+
+        let ctah_hydrostatic_pressure_change = 
+            ctah_vertical.get_hydrostatic_pressure_change(
+                fluid_temp) +
+            ctah_horizontal.get_hydrostatic_pressure_change(
+                fluid_temp);
+
+        let actual_pressure_loss = -(ctah_vertical_pressure_change.value +
+                                     ctah_horizontal_pressure_change.value -
+                                     ctah_hydrostatic_pressure_change.value);
+        // Assert that maximum error < 4%
+        assert_relative_eq!(expected_pressure_loss,
+                            actual_pressure_loss,
+                            max_relative = 0.04);
+    }
 
 
-    let ctah_vertical_pressure_change = 
-        CalcPressureChange::from_mass_rate(
-            &ctah_vertical,
-            mass_flow_expected,
-            fluid_temp);
-
-    let ctah_horizontal_pressure_change = 
-        CalcPressureChange::from_mass_rate(
-            &ctah_horizontal,
-            mass_flow_expected,
-            fluid_temp);
-
-    // let's subtract out the pressure change due to hydrostatic pressure
-    //
-    use fluid_mechanics_rust::therminol_component::
-        StandardCustomComponentProperties;
-
-    let ctah_hydrostatic_pressure_change = 
-        ctah_vertical.get_hydrostatic_pressure_change(
-            fluid_temp) +
-        ctah_horizontal.get_hydrostatic_pressure_change(
-            fluid_temp);
-
-    let ctah_reference_pressure_change = 
-        get_ctah_pressure_change_empirical(
-            mass_flow_expected,
-            fluid_temp);
-
-    let expected_pressure_loss = -(ctah_reference_pressure_change.
-        value);
 
 
 
-    let actual_pressure_loss = -(ctah_vertical_pressure_change.value +
-        ctah_horizontal_pressure_change.value -
-        ctah_hydrostatic_pressure_change.value);
 
-    assert_eq!(expected_pressure_loss,actual_pressure_loss);
+
+
 }
 
 #[test]
@@ -207,13 +218,13 @@ pub fn when_mx10_pressure_change_expect_correct_value_non_zero_flow(){
 
     use uom::si::f64::*;
 
-    // Setup
-    //
 
 
     // let's get the component for ctah
     for i in 0..19 {
 
+        // Setup
+        //
         let mx10 = factory::StaticMixer10::get();
 
         // now let's have a temperature of 21C and mass flow of 0.15 kg/s
