5e17afd619ba6c56cd3f88944f5b1fa8444d6415
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2022-09-19 17:16:27 -0700

    mx10 matches empirical data within 7% error

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -139,6 +139,61 @@ pub fn when_ctah_pressure_change_expect_correct_value_non_zero_flow(){
     assert_eq!(expected_pressure_loss,actual_pressure_loss);
 }
 
+#[test]
+pub fn when_mx10_pressure_change_expect_correct_value_non_zero_flow(){
+    //import necessary things...
+    use fluid_mechanics_rust;
+    use fluid_mechanics_rust::therminol_component::factory;
+    use uom::si::mass_rate::kilogram_per_second;
+    use uom::si::thermodynamic_temperature::degree_celsius;
+
+    use uom::si::f64::*;
+
+
+    // let's get the component for ctah
+    let mx10 = factory::StaticMixer10::get();
+
+    // now let's have a temperature of 21C and mass flow of 0.15 kg/s
+    let fluid_temp = ThermodynamicTemperature::new::<
+        degree_celsius>(20.0);
+    let mass_flow_expected = MassRate::new::<kilogram_per_second>(-0.18);
+
+    // let's get the pressure changes
+    use fluid_mechanics_rust::therminol_component::CalcPressureChange;
+
+
+    let mx10_pressure_change = 
+        CalcPressureChange::from_mass_rate(
+            &mx10,
+            mass_flow_expected,
+            fluid_temp);
+
+
+    // let's subtract out the pressure change due to hydrostatic pressure
+    //
+    use fluid_mechanics_rust::therminol_component::
+        StandardCustomComponentProperties;
+
+    let mx10_hydrostatic_pressure_change = 
+        mx10.get_hydrostatic_pressure_change(
+            fluid_temp) ;
+
+    let mx10_reference_pressure_change = 
+        get_mx10_pressure_change_empirical(
+            mass_flow_expected,
+            fluid_temp);
+
+    let expected_pressure_loss = -(mx10_reference_pressure_change.
+        value);
+
+
+
+    let actual_pressure_loss = -(mx10_pressure_change.value -
+        mx10_hydrostatic_pressure_change.value);
+
+    assert_eq!(expected_pressure_loss,actual_pressure_loss);
+}
+
 pub fn get_ctah_pressure_change_empirical(
     mass_flow_rate: MassRate,
     fluid_temp: ThermodynamicTemperature) -> Pressure {
@@ -184,6 +239,7 @@ pub fn get_m44_value(mass_flow_rate: MassRate) -> Length {
     return Length::new::<meter>(length_meter);
 
 }
+
 pub fn get_m45_value(mass_flow_rate: MassRate) -> Length {
     use uom::si::mass_rate::kilogram_per_second;
     use uom::si::length::meter;
@@ -199,4 +255,68 @@ pub fn get_m45_value(mass_flow_rate: MassRate) -> Length {
 
 }
 
+pub fn get_mx10_pressure_change_empirical(
+    mut mass_flow_rate: MassRate,
+    fluid_temp: ThermodynamicTemperature) -> Pressure {
+
+    if mass_flow_rate.value < 0.0 {
+        mass_flow_rate = mass_flow_rate * -1.0;
+    }
+
+    let delta_h = - get_m12_value(mass_flow_rate) + 
+        get_m11_value(mass_flow_rate);
+
+    let g: Acceleration = 
+        Acceleration::new::<meter_per_second_squared>(-9.81);
+
+    // now to get density
+    //import necessary things...
+    use fluid_mechanics_rust::therminol_component::
+        custom_therminol_component::DowthermACustomComponent;
+
+    use fluid_mechanics_rust::therminol_component::FluidProperties;
+    use uom::si::acceleration::meter_per_second_squared;
+
+
+    use uom::si::f64::*;
+
+
+    // let's get the component for ctah
+
+    let density =  DowthermACustomComponent::density(fluid_temp);
+
+
+    return -density*g*delta_h;
+
+}
+
+pub fn get_m11_value(mass_flow_rate: MassRate) -> Length {
+    use uom::si::mass_rate::kilogram_per_second;
+    use uom::si::length::meter;
+
+    let mass_rate_kg_per_s: f64 = mass_flow_rate.
+        get::<kilogram_per_second>();
+
+    let length_meter = 4.3053967222026 * mass_rate_kg_per_s.powf(2.0) +
+        0.267362331185577 * mass_rate_kg_per_s + 
+        1.0;
+
+    return Length::new::<meter>(length_meter);
+
+}
+
+pub fn get_m12_value(mass_flow_rate: MassRate) -> Length {
+    use uom::si::mass_rate::kilogram_per_second;
+    use uom::si::length::meter;
+
+    let mass_rate_kg_per_s: f64 = mass_flow_rate.
+        get::<kilogram_per_second>();
+
+    let length_meter = 1.58298656857859 * mass_rate_kg_per_s.powf(2.0) +
+        0.207552832049847 * mass_rate_kg_per_s + 
+        1.0;
+
+    return Length::new::<meter>(length_meter);
+
+}
 
