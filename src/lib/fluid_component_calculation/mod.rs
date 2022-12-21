c12ba18a11c1c13b4042efe85dfed5e27e389adf
tedGuest@ideapadRyzenLaptop <theodore_ong@berkeley.edu>
2022-12-20 22:49:51 -0800

    added traits for generic fluid component and specifically pipes

--- src/lib/fluid_component_calculation/mod.rs
+++ src/lib/fluid_component_calculation/mod.rs
@@ -16,3 +16,77 @@ pub mod custom_component_calc;
 
 
 
+use uom::si::f64::*;
+
+
+/// This is a fluid component trait,
+/// which specifies that fluid components in general
+/// should have the following properties accessed
+/// via get and set methods
+///
+/// ```rust
+/// ```
+pub trait FluidComponent {
+
+    /// gets the mass flowrate of the component
+    fn get_mass_flowrate(&mut self) -> MassRate ;
+
+    /// sets the mass flowrate of the component
+    fn set_mass_flowrate(&mut self, mass_flowrate: MassRate);
+
+
+    /// gets pressure loss
+    fn get_pressure_loss(&mut self) -> Pressure;
+
+    /// sets the pressure loss of the component
+    fn set_pressure_loss(&mut self, pressure_loss: Pressure);
+
+
+    /// gets cross sectional area
+    fn get_cross_sectional_area(&mut self) -> Area;
+
+    /// sets the cross sectional area of the component
+    fn set_cross_sectional_area(&mut self, cross_sectional_area: Area);
+
+
+    /// gets hydraulic diamter
+    fn get_hydraulic_diameter(&mut self) -> Length;
+
+    /// sets the hydraulic diameter of the component
+    fn set_hydraulic_diameter(&mut self, hydraulic_diameter: Length);
+
+
+    /// gets fluid viscosity
+    fn get_fluid_viscosity(&mut self) -> DynamicViscosity;
+
+    /// sets the fluid viscosity of the component
+    fn set_fluid_viscosity(&mut self, fluid_viscosity: DynamicViscosity);
+
+
+    /// gets fluid density
+    fn get_fluid_density(&mut self) -> MassDensity;
+
+    /// sets the fluid density of the component
+    fn set_fluid_density(&mut self, fluid_density: MassDensity);
+
+
+    /// gets the component length
+    fn get_component_length(&mut self) -> Length;
+
+    /// sets the component length 
+    fn set_component_length(&mut self, component_length: Length);
+
+}
+
+
+
+#[cfg(test)]
+mod fluid_component_tests {
+
+    #[test]
+    pub fn fluid_component_sandbox_1 () {
+
+    }
+
+}
+
--- src/lib/fluid_component_calculation/standard_pipe_calc.rs
+++ src/lib/fluid_component_calculation/standard_pipe_calc.rs
@@ -4,6 +4,137 @@ use crate::churchill_friction_factor;
 use crate::dimensionalisation;
 
 use uom::si::f64::*;
+/// provides generic methods to calculate mass flowrate
+/// and pressure losses for pipes
+pub trait FluidPipeCalcPressureLoss {
+
+    /// gets form loss k for a pipe
+    fn get_pipe_form_loss_k(&mut self) -> f64;
+    
+    /// sets form loss k for a pipe
+    fn set_pipe_form_loss_k(&mut self, pipe_form_loss_k: f64);
+
+    /// a function calculates pressure
+    /// loss given a mass flowrate and pipe properties
+    fn pipe_calc_pressure_loss(mut fluid_mass_flowrate: MassRate,
+                               cross_sectional_area: Area,
+                               hydraulic_diameter: Length,
+                               fluid_viscosity: DynamicViscosity,
+                               fluid_density: MassDensity,
+                               pipe_length: Length,
+                               absolute_roughness: Length,
+                               form_loss_k: f64) -> Pressure {
+        // first let's calculate roughness ratio
+
+        let roughness_ratio_quantity = absolute_roughness/hydraulic_diameter;
+
+        let roughness_ratio = 
+            dimensionalisation::convert_dimensionless_number_to_float(
+                roughness_ratio_quantity);
+
+        // second i want to take care of reverse flow
+
+        let mut reverse_flow = false;
+        if fluid_mass_flowrate.value < 0.0 {
+            reverse_flow = true;
+        }
+
+        if reverse_flow {
+            fluid_mass_flowrate = fluid_mass_flowrate * -1.0;
+        }
+
+        // and let's get the reynolds_number and L/D
+        let reynolds_number = dimensionalisation::CalcReynolds::from_mass_rate(
+            fluid_mass_flowrate,
+            cross_sectional_area,
+            hydraulic_diameter,
+            fluid_viscosity);
+
+        let length_to_diameter_ratio 
+            = dimensionalisation::convert_dimensionless_number_to_float(
+                pipe_length/hydraulic_diameter);
+
+        // then let's obtain the pipe Bejan Number
+        // given the Re
+
+        let bejan_number = churchill_friction_factor::getBe(
+            reynolds_number,
+            roughness_ratio,
+            length_to_diameter_ratio,
+            form_loss_k);
+
+        // once we get bejan_number, we can get the pressure loss terms
+        //
+        let pressure_loss = dimensionalisation::CalcBejan::to_pressure(
+            bejan_number,
+            hydraulic_diameter,
+            fluid_density,
+            fluid_viscosity);
+
+
+        // now before i exit, i want to make sure reverse flow is taken care
+        // of
+        if reverse_flow {
+            return pressure_loss * -1.0;
+        }
+
+        return pressure_loss;
+    }
+
+
+
+    /// a function which calculates pressure
+    /// loss given a mass flowrate and pipe properties
+    fn pipe_calc_mass_flowrate(pressure_loss: Pressure,
+                               cross_sectional_area: Area,
+                               hydraulic_diameter: Length,
+                               fluid_viscosity: DynamicViscosity,
+                               fluid_density: MassDensity,
+                               pipe_length: Length,
+                               absolute_roughness: Length,
+                               form_loss_k: f64) -> MassRate {
+
+        // first let's get our relevant ratios:
+        let roughness_ratio_quantity = absolute_roughness/hydraulic_diameter;
+
+        let roughness_ratio = 
+            dimensionalisation::convert_dimensionless_number_to_float(
+                roughness_ratio_quantity);
+
+        let length_to_diameter_ratio 
+            = dimensionalisation::convert_dimensionless_number_to_float(
+                pipe_length/hydraulic_diameter);
+
+        // then get Bejan number:
+
+        let bejan_number_calculated_using_diameter = 
+            dimensionalisation::CalcBejan::from_pressure(
+            pressure_loss, hydraulic_diameter, 
+            fluid_density, fluid_viscosity);
+
+        // let's get Re
+        let reynolds_number_calculated_using_diameter = 
+            churchill_friction_factor::getRe(
+                bejan_number_calculated_using_diameter,
+                roughness_ratio,
+                length_to_diameter_ratio,
+                form_loss_k);
+
+
+        // and finally return mass flowrate
+        //
+        let fluid_mass_flowrate = 
+            dimensionalisation::CalcReynolds::to_mass_rate(
+                cross_sectional_area,
+                reynolds_number_calculated_using_diameter,
+                hydraulic_diameter,
+                fluid_viscosity);
+
+        return fluid_mass_flowrate;
+
+    }
+}
+
 /// Contains functions or methods to calculate pressure loss
 /// from mass flowrate or to mass flowrate
 pub struct CalcPressureLoss {}
@@ -213,5 +344,4 @@ impl CalcPressureLoss {
 
     }
 }
-
 
